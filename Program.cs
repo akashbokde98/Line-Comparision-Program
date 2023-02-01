3254a73be8a88948d5e1a4037f469f397e869159
akashbokde98 <akashbokde98@gmail.com>
2023-02-01 11:35:22 +0530

    Initial Push

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LineComparision
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Line Comparision Compustation Program!");
+        }
+    }
+}
